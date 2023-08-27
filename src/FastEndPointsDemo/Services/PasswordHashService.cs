5203aa7da03a9a547c9a7e38d51836c5a4b5fde5
henrychris <64085210+henrychris@users.noreply.github.com>
2023-08-27 21:01:39 +0100

    add service to hashPasswords

---
+++ src/FastEndPointsDemo/Services/PasswordHashService.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using FastEndPointsDemo.Interfaces;
+
+namespace FastEndPointsDemo.Services;
+
+public class PasswordHashService : IPasswordHashService
+{
+    public string HashPassword(string password)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(password);
+        var hash = sha256.ComputeHash(bytes);
+
+        return Convert.ToBase64String(hash);
+    }
+
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        var hashedInput = HashPassword(password);
+        return hashedInput == hashedPassword;
+    }
+}
