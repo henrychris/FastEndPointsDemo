b74ab8a262fab4fd17f6876885ed394df2e389db
henrychris <64085210+henrychris@users.noreply.github.com>
2023-08-27 21:01:25 +0100

    add mappingProfile

---
+++ src/FastEndPointsDemo/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FastEndPointsDemo.Data.Entities;
+using FastEndPointsDemo.Endpoints.RequestModels;
+using FastEndPointsDemo.Endpoints.ResponseModels;
+
+namespace FastEndPointsDemo;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<User, LoginResponse>();
+        CreateMap<RegisterRequest, User>()
+            .ForMember(d => d.UserName, opt => opt.MapFrom(src => src.EmailAddress))
+            .ForMember(d => d.EmailAddress, opt => opt.MapFrom(src => src.EmailAddress));
+        CreateMap<User, RegisterResponse>();
+    }
+}
