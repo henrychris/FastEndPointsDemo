b9b03281db923420ac43d7fd0a49a29dd19c2915
henrychris <64085210+henrychris@users.noreply.github.com>
2023-08-27 21:01:17 +0100

    add UtilityService

---
+++ src/FastEndPointsDemo/Interfaces/IUtilityService.cs
@@ -0,0 +1,20 @@
+﻿using FastEndPointsDemo.Models;
+
+namespace FastEndPointsDemo.Interfaces;
+
+public interface IUtilityService
+{
+    /// <summary>
+    /// Validate that a password passes strength requirements.
+    /// </summary>
+    /// <param name="password">The password to validate.</param>
+    /// <returns></returns>
+    ServiceResponse<bool> ValidatePassword(string password);
+
+    /// <summary>
+    /// Format a phone number to contain country code.
+    /// </summary>
+    /// <param name="phoneNumber"></param>
+    /// <returns></returns>
+    string FormatPhoneNumber(string phoneNumber);
+}
---
+++ src/FastEndPointsDemo/Services/UtilityService.cs
@@ -0,0 +1,19 @@
+﻿using FastEndPointsDemo.Interfaces;
+using FastEndPointsDemo.Models;
+
+namespace FastEndPointsDemo.Services;
+
+public class UtilityService : IUtilityService
+{
+    public ServiceResponse<bool> ValidatePassword(string password)
+    {
+        // could do password validation in the endpoint?
+        throw new NotImplementedException();
+    }
+
+    public string FormatPhoneNumber(string phoneNumber)
+    {
+        var tenDigitPhoneNumber = phoneNumber.Substring(phoneNumber.Length - 10, 10);
+        return $"234{tenDigitPhoneNumber}";
+    }
+}
