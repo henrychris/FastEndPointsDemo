7686a8934936499d7f62ca8d8c1efc6b9bf13732
henrychris <64085210+henrychris@users.noreply.github.com>
2023-08-27 21:02:15 +0100

    update appsettings and nugets

---
+++ src/FastEndPointsDemo/Models/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace FastEndPointsDemo.Models;
+
+public class JwtSettings
+{
+    public string Secret { get; set; }
+    public string Audience { get; set; }
+    public string Issuer { get; set; }
+    public int LifeSpan { get; set; }
+}
