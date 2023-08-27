5a2ce5641c857cd2fb87e3ec362725eef1f4814e
henrychris <64085210+henrychris@users.noreply.github.com>
2023-08-27 21:01:04 +0100

    add tokenService

---
+++ src/FastEndPointsDemo/Interfaces/ITokenService.cs
@@ -0,0 +1,6 @@
+﻿namespace FastEndPointsDemo.Interfaces;
+
+public interface ITokenService
+{
+    public string CreateAccessToken(string userId, string firstName, string emailAddress, string role);
+}
---
+++ src/FastEndPointsDemo/Services/TokenService.cs
@@ -0,0 +1,43 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FastEndPointsDemo.Interfaces;
+using FastEndPointsDemo.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FastEndPointsDemo.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly JwtSettings _jwtSettings;
+
+    public TokenService(IOptionsSnapshot<JwtSettings> jwtSettings)
+    {
+        _jwtSettings = jwtSettings.Value;
+    }
+
+    public string CreateAccessToken(string userId, string firstName, string emailAddress, string role)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var secretKey = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(
+                new Claim[]
+                {
+                    new(ClaimTypes.Name, firstName),
+                    new("Email", emailAddress),
+                    new("UserId", userId),
+                    new("UserRole", role)
+                }),
+            Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.LifeSpan),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(secretKey),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+    }
+}
