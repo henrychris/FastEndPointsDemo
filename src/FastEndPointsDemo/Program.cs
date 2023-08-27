2d0fa7c5f63cd46656a03a5e093caef68f850eb7
henrychris <64085210+henrychris@users.noreply.github.com>
2023-08-27 21:02:35 +0100

    update program.cs with services

--- src/FastEndPointsDemo/Program.cs
+++ src/FastEndPointsDemo/Program.cs
@@ -1,12 +1,60 @@
-﻿using FastEndpoints;
+﻿using System.Reflection;
+using System.Text;
+using FastEndpoints;
 using FastEndpoints.Swagger;
-using FastEndPointsDemo;
+using FastEndPointsDemo.Data;
+using FastEndPointsDemo.Interfaces;
+using FastEndPointsDemo.Models;
+using FastEndPointsDemo.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
 
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddFastEndpoints().SwaggerDocument();
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.RequireHttpsMetadata = false;
+    o.SaveToken = true;
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey =
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["JwtSettings:Secret"] ??
+                                                             throw new InvalidOperationException(
+                                                                 "Signing Key is null!"))),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+        ValidIssuer = configuration["JwtSettings:Issuer"],
+        ValidAudience = configuration["JwtSettings:Audience"],
+    };
+});
+
+// services
+builder.Services.AddDbContext<DataContext>(
+    o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")),
+    ServiceLifetime.Transient);
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUtilityService, UtilityService>();
+builder.Services.AddSingleton<IPasswordHashService, PasswordHashService>();
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseFastEndpoints().UseSwaggerGen().UseDefaultExceptionHandler();
 
 app.Run();
