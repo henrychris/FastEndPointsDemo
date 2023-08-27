0f9dda1cb75f7892f50cdd4c052be756fa8959ab
henrychris <64085210+henrychris@users.noreply.github.com>
2023-08-27 20:57:50 +0100

    add db entities

---
+++ src/FastEndPointsDemo/Data/Entities/Business.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastEndPointsDemo.Data.Entities;
+
+public class Business
+{
+    [Key]
+    public string Code { get; set; }
+}
---
+++ src/FastEndPointsDemo/Data/Entities/User.cs
@@ -0,0 +1,49 @@
+﻿using FastEndPointsDemo.Models;
+
+namespace FastEndPointsDemo.Data.Entities;
+
+public class User
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string FirstName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string LastName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string UserName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string EmailAddress { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Status { get; set; } = Models.Status.Active.ToString();
+
+    /// <summary>
+    /// The hash representation of a user's password
+    /// </summary>
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public string Role { get; set; } = string.Empty;
+    public string ReferralCode { get; set; } = string.Empty;
+}
